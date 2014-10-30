e423e4ad41171a1379dad0b0b7360400d94c380b
root <jeffs.fernandes@gmail.com>
2014-10-30 13:48:16 -0200

    pagina de explicacao

---
+++ creches/Site/cores_do_mapa.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Cuidando do meu Bairro Creches</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+		<!--favicon-->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
+
+    <!--CSS Styles-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/docs.css" rel="stylesheet">
+    <!--END OF CSS Styles-->
+    
+    <!--Javascript-->
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/application.js"></script>
+		<script src="js/jquery.validate.js"></script>
+		<script src="js/form-validator.js"></script>
+  </head>
+
+  <body>
+    <?php include("header.inc.php"); ?>
+    
+    <div class="container">
+      	<div class="row">
+      		<div class="span7">
+      			<h1>Em construcao</h1>
+
+      		</div>
+      	</div> <!--Row-->
+    </div> <!--Container-->
+
+    <?php include("footer.inc.php"); ?>
+  </body>
+</html>
