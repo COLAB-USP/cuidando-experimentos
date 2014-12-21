96c38700875e693e80abf526b5c1e1481273ce4f
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2014-12-21 16:33:20 -0200

    Texto de explicação das cores do mapa

--- creches/Site/cores_do_mapa.php
+++ creches/Site/cores_do_mapa.php
@@ -18,7 +18,7 @@
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="css/docs.css" rel="stylesheet">
     <!--END OF CSS Styles-->
-    
+
     <!--Javascript-->
 		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -29,12 +29,16 @@
 
   <body>
     <?php include("header.inc.php"); ?>
-    
+
     <div class="container">
       	<div class="row">
       		<div class="span7">
-      			<h1>Em construcao</h1>
-
+      			<h1>Cores do mapa</h1>
+            <p>As cores do mapa indicam quão grande é a fila por vagas em creches no distrito.</p>
+            <p>O tamanho da fila é extraído da página da <a href="http://eolgerenciamento.prefeitura.sp.gov.br/se1426g/frmgerencial/ConsultaCandidatosCadastrados.aspx?Cod=000000" target="_blank">Secretaria Municipal de Educação</a>.</p>
+            <p>Para calcular a <b>fila per capta</b> do distrito ainda são utilizados os tamanhos da população infantil desses distritos. O tamanho da população infantul é fornecido pela <a href="http://produtos.seade.gov.br/produtos/distritos/index.php" target="_blank">Fundação SEADE</a>.</p>
+            <p>A <b>fila per capta</b> é calculada dividindo-se o tamanho da fila pelo tamanho da população (e multiplicando o valor por 100).</p>
+            <p>Os valores são atualizados diariamente.</p>
       		</div>
       	</div> <!--Row-->
     </div> <!--Container-->
